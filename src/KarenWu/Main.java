45133b2d9bb47130c6a97908a7425c0a936c1a16
BT_1N3_27 <kwu8742@bths.edu>
2018-10-03 08:31:32 -0400

    Testing

--- src/KarenWu/Main.java
+++ src/KarenWu/Main.java
@@ -6,6 +6,7 @@ public class Main {
 	    System.out.println(KWuLib.isPalindrome);
         System.out.println(KWuLib.dateStr);
         System.out.println(KWuLib.fooBarBaz);
-
+        System.out.println(KWuLib.StringUnion);
+        System.out.println(KWuLib.isFibonnaci);
     }
 }
